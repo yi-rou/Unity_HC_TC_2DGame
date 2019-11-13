4a7cb2fd4ac59b8c7f12f4399d8eedad9b04f1ef
yi-rou <57440062+yi-rou@users.noreply.github.com>
2019-11-13 21:37:34 +0800

    地板會動

---
+++ UnityProject/Assets/Scripts/API.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    public Transform bird;
+
+    private void Start()
+    {
+        Debug.Log("ya");
+        Debug.LogWarning("warning!!!");
+        Debug.LogError("NO");
+
+        Debug.Log(Mathf.PI);
+
+        Debug.Log(bird.position);
+    }
+
+}
--- UnityProject/Assets/Scripts/Floor.cs
+++ UnityProject/Assets/Scripts/Floor.cs
@@ -4,4 +4,14 @@ public class Floor : MonoBehaviour
 {
     [Header("地板速度"),Range(0.5f,99.9f)]
     public int speed = 1;
+
+    public Transform floor;
+
+    private void Update()
+    {
+        floor.Translate(-speed,0,0);
+    }
+
+
+
 }
