5c409d9af70b83a27d13fd5f8658754461864bb8
yi-rou <57440062+yi-rou@users.noreply.github.com>
2019-11-22 21:42:28 +0800

    LearnOperator

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool open;
+
+    public int score;
+
+    public int combo;
+    private void Start()
+    {
+        // 判斷式 if
+        // if (布林值) {當布林值為true才會執行 }
+        if (true)
+        {
+            print("fly");
+        }
+        // 布林值結果為false,才會執行else的{}
+        if (open)
+        {
+            print("開啟");
+        }
+        else
+        {
+            print("關閉");
+        }
+
+        if (score >= 60)
+        {
+            print("恭喜");
+        }
+        else if (score >= 40)
+        {
+            print("再接再厲");
+        }
+        else
+        {
+            print("明年再來吧");
+        }
+        if (combo < 50)
+        {
+            print("攻擊");
+        }
+        else if (combo < 100)
+        {
+            print("攻擊*2");
+        }
+        else if (combo < 150)
+        {
+            print("攻擊*5");
+        }
+        else
+        {
+            print("攻擊*10");
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int a = 55, b = 6;
+    public int c = 3, d = 9;
+    private void Start()
+    {
+        #region 數學運算子
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+
+        a = a + 1;
+        print(a);
+
+        print(a++);   //先輸出 a 再遞增
+        print(++a);   //先遞增再輸出 a
+        print(b--);
+        print(--b);
+
+        print(b += 10);  // b + 10=簡寫成 b += 10
+        #endregion
+        #region  比較運算子 
+        print(c > d);   //傳回都是布林值
+        print(c < d);
+        print(c >= d);
+        print(c <= d);
+        print(c == d);  // == 等於
+        print(c != d);  // != 不等於
+        #endregion
+
+        #region  邏輯運算子
+        // && 並且
+        // 只要有一個false就會傳回false
+        print(true && true);
+        print(true && false);
+        print(false && true);
+        print(false && false);
+        // || 或者
+        // 只要一個true就會傳回true
+        print(true || true);
+        print(true || false);
+        print(false || true);
+        print(false || false);
+
+        // ! 相反
+        print(!true);
+        print(!false);
+        #endregion
+    }
+}
